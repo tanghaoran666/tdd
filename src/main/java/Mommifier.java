f0fc04f42c4bd2d499f70edaa74765ff4f5b6c26
tanghaoran <407458281@qq.com>
2020-09-14 11:58:19 +0800

    implement return origin strings when convert given strings has vowels but not continuous

---
+++ src/main/java/Mommifier.java
@@ -0,0 +1,5 @@
+public class Mommifier {
+    public String convert(String strings) {
+        return strings;
+    }
+}
--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -1,2 +1,24 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class MommifierTest {
+    Mommifier mommifier;
+    @BeforeEach
+    void setUp(){
+        mommifier = new Mommifier();
+    }
+
+    @Test
+    void should_return_origin_strings_when_convert_given_strings_has_vowels_but_not_continuous(){
+        String strings = "abedijoku";
+        String result = mommifier.convert(strings);
+        assertEquals("abedijoku",result);
+    }
+
+
 }
